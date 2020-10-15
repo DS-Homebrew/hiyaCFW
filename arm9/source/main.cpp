2c08b678507d54f252c161849938c0e66b7cd06b
Pk11 <epicpkmn11@outlook.com>
2020-10-15 15:08:17 -0500

    Add dummy file creation

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -1,6 +1,8 @@
+#include <dirent.h>
 #include <nds.h>
 #include <slim.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #include "bios_decompress_callback.h"
 #include "fileOperations.h"
@@ -275,6 +277,39 @@ int main( int argc, char **argv) {
 				saveSettings();
 				break;
 			}
+		}
+	}
+
+	// Create dummy file
+	// Check the free space
+	struct statvfs st;
+	statvfs("sd:/", &st);
+	u32 freeSpace = st.f_bsize * st.f_bfree;
+
+	// If the free space is bigger than 2GiB (using a u32 so always 0 - 4GiB)
+	if(freeSpace > (2u << 30)) {
+		consoleDemoInit();
+		// Remove old dummy file if it exists and recheck free space
+		if(access("sd:/hiya/dummy.bin", F_OK) == 0) {
+			printf("Removing old dummy file... ");
+			remove("sd:/hiya/dummy.bin");
+			printf("Done!\n");
+		}
+		statvfs("sd:/", &st);
+		freeSpace = st.f_bsize * st.f_bfree;
+
+		// Make sure hiya directory exists and make the file
+		mkdir("sd:/hiya", 0777);
+		FILE *file = fopen("sd:/hiya/dummy.bin", "wb");
+		if(file) {
+			// Free space - 2GB + 10MB
+			printf("Making new dummy file...   ");
+			fseek(file, (freeSpace - (2 << 30)) + 10000000, SEEK_SET);
+			fputc('\0', file);
+			fclose(file);
+			printf("Done!\n");
+		} else {
+			printf("Failed to open file!\n");
 		}
 	}
 
