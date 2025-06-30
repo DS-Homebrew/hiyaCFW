8ead63f26d58e7167b0d556edb548fe06511fb0b
RocketRobz <bobesh8@gmail.com>
2025-06-29 18:07:20 -0600

    Display GIF splashes with RGB565 color space

--- arm9/source/gif.cpp
+++ arm9/source/gif.cpp
@@ -46,7 +46,6 @@ void Gif::displayFrame(void) {
 	std::vector<u16> &colorTable = frame.descriptor.lctFlag ? frame.lct : _gct;
 
 	tonccpy(_top ? BG_PALETTE : BG_PALETTE_SUB, colorTable.data(), colorTable.size() * 2);
-	tonccpy(_top ? BG_PALETTE : BG_PALETTE_SUB, colorTable.data(), colorTable.size() * 2);
 
 	// Disposal method 2 = fill with bg color
 	if (frame.gce.disposalMethod == 2)
@@ -123,7 +122,20 @@ bool Gif::load(const char *path, bool top, bool animate, bool forceDecompress) {
 
 		_gct = std::vector<u16>(numColors);
 		for (int i = 0; i < numColors; i++) {
-			_gct[i] = fgetc(file) >> 3 | (fgetc(file) >> 3) << 5 | (fgetc(file) >> 3) << 10 | BIT(15);
+			const u8 r = fgetc(file);
+			const u8 g = fgetc(file);
+			const u8 b = fgetc(file);
+
+			const u16 green = (g >> 2) << 5;
+			_gct[i] = r >> 3 | (b >> 3) << 10;
+			if (green & BIT(5)) {
+				_gct[i] |= BIT(15);
+			}
+			for (int gBit = 6; gBit <= 10; gBit++) {
+				if (green & BIT(gBit)) {
+					_gct[i] |= BIT(gBit-1);
+				}
+			}
 		}
 	}
 
@@ -185,7 +197,20 @@ bool Gif::load(const char *path, bool top, bool animate, bool forceDecompress) {
 					int numColors = 2 << frame.descriptor.lctSize;
 					frame.lct = std::vector<u16>(numColors);
 					for (int i = 0; i < numColors; i++) {
-						frame.lct[i] = fgetc(file) >> 3 | (fgetc(file) >> 3) << 5 | (fgetc(file) >> 3) << 10 | BIT(15);
+						const u8 r = fgetc(file);
+						const u8 g = fgetc(file);
+						const u8 b = fgetc(file);
+
+						const u16 green = (g >> 2) << 5;
+						frame.lct[i] = r >> 3 | (b >> 3) << 10;
+						if (green & BIT(5)) {
+							frame.lct[i] |= BIT(15);
+						}
+						for (int gBit = 6; gBit <= 10; gBit++) {
+							if (green & BIT(gBit)) {
+								frame.lct[i] |= BIT(gBit-1);
+							}
+						}
 					}
 				}
 
