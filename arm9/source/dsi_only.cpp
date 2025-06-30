18fceeaf735644116229a7727456f5d26b559cdf
RocketRobz <bobesh8@gmail.com>
2025-06-29 18:16:17 -0600

    Update dsi_only.cpp

--- arm9/source/dsi_only.cpp
+++ arm9/source/dsi_only.cpp
@@ -12,7 +12,7 @@ static void dsiOnly_setBrightness(u8 screen, s8 bright) {
 	if (bright > 31) {
 		bright = 31;
 	}
-	*(u16*)(0x0400006C + (0x1000 * screen)) = bright + mode;
+	*(vu16*)(0x0400006C + (0x1000 * screen)) = bright + mode;
 }
 
 void dsiOnly(void) {
